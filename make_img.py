9db7764384e70feffdc091bf52144b0ec17907a2
dczheng <dczheng21@outlook.com>
2020-08-04 16:50:58 +0800

    update

--- make_img.py
+++ make_img.py
@@ -13,11 +13,27 @@ if len(t) < 2:
     print( "ERROR" )
     sys.exit(1)
 
-if t[-1] == "png":
-    sys.exit(0)
+if t[-1] != "png":
+    fn_out = ''.join( t[:-1] ) + '.png'
+    print( "%s -> %s."%(fn, fn_out) )
 
-fn_out = ''.join( t[:-1] ) + '.png'
-print( "%s -> %s."%(fn, fn_out) )
+    im.save( fn_out, 'png' )
+    os.remove( fn )
 
-im.save( fn_out, 'png' )
-os.remove( fn )
+print( im.size, ' ',  end='' )
+
+w, h = im.size
+r = float(w) / h
+x = 500
+if w > x or h > x:
+    if w > h:
+        ww = x 
+        hh = int( ww / r )
+    else:
+        hh = x
+        ww = int( hh * r )
+    im = im.resize( (ww, hh), Image.ANTIALIAS )
+
+print( im.size )
+fn_thumb = ''.join( t[:-1] ) + '-thumb.png'
+im.save( fn_thumb, 'png' )
